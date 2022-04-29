64bb8bfb609637a8da7515fdee44d3da4d3d0b0b
TomasYanez186 <toyanez61@gmail.com>
2022-04-29 12:38:54 -0400

    Arreglar error de caps lock al ingresar nombre de archivo.

--- Cuenta-ficha/cuenta.py
+++ Cuenta-ficha/cuenta.py
@@ -182,7 +182,11 @@ def ventanaEncontrada(nom_modulo, fecha):
     ag.press("enter")
     # time.sleep(2)
     ag.click(770, 904)
-    ag.write('Cuenta-ficha_' + nom_modulo + '_' + str(fecha) + '.xlsx')
+    caps3 = GetKeyState(VK_CAPITAL)
+    if caps3 == 0:
+        ag.write('Cuenta-ficha_' + nom_modulo + '_' + str(fecha) + '.xlsx')
+    else:
+        ag.write('cUENTA-FICHA_' + nom_modulo.lower() + '_' + str(fecha) + '.XLSX')
     ag.click(771, 925)
     ag.write("ll")
     ag.press("enter")
